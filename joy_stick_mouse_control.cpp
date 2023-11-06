036a5c844f3d343182281c8a03818d70e7b7d7ad
KyawSawLIN <linandrew48@gmail.com>
2023-11-06 14:35:59 +0800

    add backspace key

--- joy_stick_mouse_control.cpp
+++ joy_stick_mouse_control.cpp
@@ -1,39 +1,10 @@
-/*
-  JoystickMouseControl
-
-  Controls the mouse from a joystick on an Arduino Leonardo, Micro or Due.
-  Uses a pushbutton to turn on and off mouse control, and a second pushbutton
-  to click the left mouse button.
-
-  Hardware:
-  - 2-axis joystick connected to pins A0 and A1
-  - pushbuttons connected to pin D2 and D3
-
-  The mouse movement is always relative. This sketch reads two  inputs
-  ranges of -6 to 6.
-  The sketch assumes that the joystick resting values are around the middle ofanalog
-  the range, but that they vary within a threshold.
-
-  WARNING: When you use the Mouse.move() command, the Arduino takes over your
-  mouse! Make sure you have control before you use the command. This sketch
-  includes a pushbutton to toggle the mouse control state, so you can turn on
-  and off mouse control.
-
-  created 15 Sep 2011
-  updated 28 Mar 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/JoystickMouseControl
-*/
-
 #include "Mouse.h"
 #include "Keyboard.h"
 
 // set pin numbers for switch, joystick axes, and LED:
-const int switchPin = 2;    // switch to turn on and off mouse control
+const int switchPin = 12;    // switch to turn on and off mouse control
 const int mouseButton = 3;  // input pin for the mouse pushButton
+const int deleteButton = 2;  // backspace button
 const int xAxis = A0;       // joystick X axis
 const int yAxis = A1;       // joystick Y axis
 const int ledPin = 13;       // Mouse control LED
@@ -60,49 +31,47 @@ void setup() {
 }
 
 void loop() {
-  // read the switch:
   int switchState = digitalRead(switchPin);
-  // if it's changed and it's high, toggle the mouse state:
   if (switchState != lastSwitchState) {
     if (switchState == LOW) {
       mouseIsActive = !mouseIsActive;
-      // turn on LED to indicate mouse state:
       digitalWrite(ledPin, mouseIsActive);
     }
   }
-  // save switch state for next comparison:
   lastSwitchState = switchState;
 
-  // read and scale the two axes:
+  
+
   int xReading = readAxis(A0);
   int yReading = readAxis(A1);
 
-  // if the mouse control state is active, move the mouse:
   if (mouseIsActive) {
     Mouse.move(xReading, -yReading, 0);
   }
-
-  if (digitalRead(mouseButton) == HIGH) {
-    // if the mouse is not pressed, press it:
+  
+  if (digitalRead(mouseButton) == LOW) {
     if (!Mouse.isPressed(MOUSE_LEFT)) {
       Mouse.press(MOUSE_LEFT);
     }
   }
-  // else the mouse button is not pressed:
   else {
-    // if the mouse is pressed, release it:
     if (Mouse.isPressed(MOUSE_LEFT)) {
       Mouse.release(MOUSE_LEFT);
     }
   }
 
+  
+  if ( digitalRead(deleteButton) == LOW)
+  {
+    delay(200);
+    Keyboard.press(backspace);
+    Keyboard.release(backspace);
+    delay(10);
+  }
+  
+
   delay(responseDelay);
 }
-
-/*
-  reads an axis (0 or 1 for x or y) and scales the analog input range to a range
-  from 0 to <range>
-*/
 
 int readAxis(int thisAxis) {
   // read the analog input:
@@ -120,4 +89,4 @@ int readAxis(int thisAxis) {
 
   // return the distance for this axis:
   return distance;
-}
+}  
