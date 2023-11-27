c0ef3582fbbe064787ca229516f24cdf440df473
KyawSawLIN <linandrew48@gmail.com>
2023-11-27 22:38:14 +0800

    update

--- websearch.py
+++ websearch.py
@@ -1,6 +1,7 @@
 import webbrowser
 import pytesseract
 from PIL import Image
+import os
 
 def search_online(query):
     search_url = "https://scholar.google.com/scholar?q={}".format(query)   
@@ -11,11 +12,39 @@ def search_online(query):
     webbrowser.get('chrome').open_new_tab(search_url)
 
 
-image_path = "b.png"  # Replace with the path to your image file
-image = Image.open(image_path)
 
-# Apply OCR to the image and retrieve the text
-text = pytesseract.image_to_string(image)
-# Example usage
+# Get the path to the folder containing the files
+folder_path = "./"  # Replace with the path to your folder
+
+# Get the list of files in the folder
+files = os.listdir(folder_path)
+
+# Iterate over the files to find the image file
+for file in files:
+    file_path = os.path.join(folder_path, file)
+    
+    # Check if the file is an image
+    if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+        try:
+            # Open the image file
+            image = Image.open(file_path)
+
+            # Apply OCR to the image and retrieve the text
+            text = pytesseract.image_to_string(image)
+
+            # Print or process the extracted text
+            print(text)
+            
+            # Break the loop after processing the first image file
+            break
+        
+        except OSError:
+            print(f"Unable to process file: {file_path}")
+    
+if 'image' not in locals():
+    print("No image file found in the folder.")
+
 search_query = text
 search_online(search_query)
+# Delete the image file
+os.remove(file_path)
